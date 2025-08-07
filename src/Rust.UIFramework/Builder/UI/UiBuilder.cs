991c384aa7b4e5b209c60920a2f6af372e76c2e4
tofurahie <tofurahie@gmail.com>
2025-08-07 17:58:18 +0300

    Added randomNames (temporary)

--- src/Rust.UIFramework/Builder/UI/UiBuilder.cs
+++ src/Rust.UIFramework/Builder/UI/UiBuilder.cs
@@ -1,4 +1,7 @@
-﻿using Oxide.Ext.UiFramework.Builder.Cached;
+﻿using System;
+using System.Buffers.Binary;
+using System.Threading;
+using Oxide.Ext.UiFramework.Builder.Cached;
 using Oxide.Ext.UiFramework.Cache;
 using Oxide.Ext.UiFramework.Exceptions;
 using Oxide.Ext.UiFramework.Json;
@@ -8,8 +11,11 @@ namespace Oxide.Ext.UiFramework.Builder.UI;
 
 public partial class UiBuilder : BaseUiBuilder
 {
+    private static long s_uniqueId = DateTime.UtcNow.Ticks;
+    
     public BaseUiComponent Root;
 
+    private bool _randomNames;
     private bool _needsMouse;
     private bool _needsKeyboard;
     private bool _autoDestroy = true;
@@ -48,6 +54,11 @@ public partial class UiBuilder : BaseUiBuilder
         _needsKeyboard = enabled;
     }
 
+    public void UseRandomNames(bool enable = true)
+    {
+        _randomNames = enable;
+    }
+    
     public void EnableAutoDestroy(bool enabled = true)
     {
         _autoDestroy = enabled;
@@ -73,19 +84,40 @@ public partial class UiBuilder : BaseUiBuilder
         return cached;
     }
     #endregion
+
+#region Methods
+
+    private static string GenerateUniqueString()
+    {
+        long id = Interlocked.Increment(ref s_uniqueId);
+        byte[] bytes = BitConverter.GetBytes(id);
+        char[] chars = new char[bytes.Length * 2];
+
+        const string hexChars = "0123456789ABCDEF";
+
+        for (int i = 0; i < bytes.Length; i++)
+        {
+            chars[i * 2] = hexChars[bytes[i] >> 4];
+            chars[i * 2 + 1] = hexChars[bytes[i] & 0xF];
+        }
+
+        return new string(chars);
+    }
+
+#endregion
         
-    #region Add Components
+#region Add Components
     public override void AddComponent(BaseUiComponent component, in UiReference parent)
     {
         UiReferenceException.ThrowIfInvalidParent(parent);
-        component.Reference = parent.WithChild(UiNameCache.GetComponentName(RootName, Components.Count));
+        component.Reference = parent.WithChild(_randomNames ? GenerateUniqueString() : UiNameCache.GetAnchorName(RootName, Anchors.Count));
         Components.Add(component);
     }
         
     protected override void AddAnchor(BaseUiComponent component, in UiReference parent)
     {
         UiReferenceException.ThrowIfInvalidParent(parent);
-        component.Reference = parent.WithChild(UiNameCache.GetAnchorName(RootName, Anchors.Count));
+        component.Reference = parent.WithChild(_randomNames ? GenerateUniqueString() : UiNameCache.GetAnchorName(RootName, Anchors.Count));
         Anchors.Add(component);
     }
     #endregion
